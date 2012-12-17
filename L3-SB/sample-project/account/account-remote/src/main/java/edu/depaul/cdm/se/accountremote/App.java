c8113cdac05f414d61da7db793f84ba20a008cac
Ken Yu <kyu_23@yahoo.com>
2012-12-16 21:11:00 -0600

    Week 3 demo (account project)

---
+++ L3-SB/sample-project/account/account-remote/src/main/java/edu/depaul/cdm/se/accountremote/App.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se.accountremote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
