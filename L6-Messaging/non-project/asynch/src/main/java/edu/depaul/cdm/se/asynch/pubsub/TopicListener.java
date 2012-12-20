994e0749b8238e37f1003bf40292f78d75ad8b07
Ken Yu <kyu_23@yahoo.com>
2012-12-20 04:46:39 -0600

    Adding Topic and Pubsub functionality

---
+++ L6-Messaging/non-project/asynch/src/main/java/edu/depaul/cdm/se/asynch/pubsub/TopicListener.java
@@ -0,0 +1,29 @@
+package edu.depaul.cdm.se.asynch.pubsub;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(mappedName = "jms/javaee6/Topic", activationConfig = {
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "TopicListener"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "TopicListener")
+})
+public class TopicListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            System.out.println("Message received: " + textMessage.getText());
+        } catch (JMSException ex) {
+            Logger.getLogger(TopicListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
