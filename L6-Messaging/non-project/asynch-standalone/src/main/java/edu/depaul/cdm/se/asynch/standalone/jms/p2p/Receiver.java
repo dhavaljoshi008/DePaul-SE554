497a39c1ff7f8b9f21da07aa1284a38286f0a34c
Ken Yu <kyu_23@yahoo.com>
2012-12-20 04:36:51 -0600

    Adding Queue and P2P functionality

---
+++ L6-Messaging/non-project/asynch-standalone/src/main/java/edu/depaul/cdm/se/asynch/standalone/jms/p2p/Receiver.java
@@ -0,0 +1,39 @@
+package edu.depaul.cdm.se.asynch.standalone.jms.p2p;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class Receiver {
+
+    public static void main(String[] args) throws Exception {
+        Context jndiContext = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("jms/javaee6/ConnectionFactory");
+        Queue queue = (Queue) jndiContext.lookup("jms/javaee6/Queue");
+
+        Connection connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        MessageConsumer consumer = session.createConsumer(queue);
+        connection.start();
+        System.out.println("P2P Dr. Fraiser is listening...");
+        while (true) {
+            TextMessage message = (TextMessage) consumer.receive();
+            System.out.println("Message received: " + message.getText());
+
+            Destination replyDestination = message.getJMSReplyTo();
+
+            // Check to see if reply was requested
+            if (replyDestination != null) {
+                session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                MessageProducer replyProducer = session.createProducer(message.getJMSReplyTo());
+                TextMessage replyMessage = session.createTextMessage("Thank you");
+                replyMessage.setJMSCorrelationID(message.getJMSMessageID());
+                replyProducer.send(replyMessage);
+                System.out.println("Message sent");
+            }
+        }
+
+        //session.close();
+        //connection.close();
+    }
+}
---
+++ L6-Messaging/non-project/asynch/src/main/java/edu/depaul/cdm/se/asynch/p2p/QueueListener.java
@@ -0,0 +1,33 @@
+package edu.depaul.cdm.se.asynch.p2p;
+
+import edu.depaul.cdm.se.sbejb.impl.SimpleGreeterBean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(mappedName = "jms/javaee6/Queue", activationConfig = {
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class QueueListener implements MessageListener {
+
+    @EJB
+    private SimpleGreeterBean greeter;
+    
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            System.out.println("Message received: " + textMessage.getText());
+            System.out.println(greeter.greetMe(textMessage.getText()));
+        } catch (JMSException ex) {
+            Logger.getLogger(QueueListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
