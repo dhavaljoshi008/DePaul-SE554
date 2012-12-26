1b38f2221db1956c8c9f959690fef16fa6c0c98a
Ken Yu <kyu_23@yahoo.com>
2012-12-26 07:33:56 -0600

    Renamed GreeterService to Greeter to setup the naming difference on the @WebService

---
+++ L7-SOAP/non-project/SOAP/src/main/java/edu/depaul/cdm/se/soap/Greeter.java
@@ -0,0 +1,24 @@
+package edu.depaul.cdm.se.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * Simpliest example of Web Service 
+ * Some enhancement opportunity
+ * @Todo - what are different attributes on each of the annotation and the
+ * impact on the wsdl?
+ * 1) @WebServices
+ * 2) @WebMethod 
+ * 3) @WebParam
+ * Web service as regular java object
+ */
+@WebService
+public class Greeter {
+    @WebMethod(action="hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
