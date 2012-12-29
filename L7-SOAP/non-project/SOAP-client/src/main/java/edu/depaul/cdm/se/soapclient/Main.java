d175e7884da4d201223a1c5d67d4eaa7569904f4
Ken Yu <kyu_23@yahoo.com>
2012-12-29 10:27:37 -0600

    Updated to include Addressed example

--- L7-SOAP/non-project/SOAP-client/src/main/java/edu/depaul/cdm/se/soapclient/Main.java
+++ L7-SOAP/non-project/SOAP-client/src/main/java/edu/depaul/cdm/se/soapclient/Main.java
@@ -1,11 +1,24 @@
 package edu.depaul.cdm.se.soapclient;
 
+import edu.depaul.cdm.se.addressedgreetings.AddressedGreeterService;
+import edu.depaul.cdm.se.addressedgreetings.AddressedGreeterService_Service;
 import edu.depaul.cdm.se.soap.GreeterService;
 import edu.depaul.cdm.se.soap.GreeterService_Service;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        Main client = new Main();
+//        client.doGreet();
+        client.doAddressedGreet();
+    }
+    
+    public void doGreet() {
         GreeterService service = new GreeterService_Service().getGreeterServicePort();
+        System.out.println(service.hello("Dave"));
+    }
+    
+    public void doAddressedGreet() {
+        AddressedGreeterService service = new AddressedGreeterService_Service().getAddressedGreeterServicePort();
         System.out.println(service.hello("Dave"));
     }
 }
