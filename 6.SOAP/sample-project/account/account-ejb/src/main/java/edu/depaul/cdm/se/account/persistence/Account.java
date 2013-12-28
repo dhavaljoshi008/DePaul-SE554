71d22cf3b12073953b58ec33390dbef37ea6fdae
shinabro <kyu_23@yahoo.com>
2013-12-28 06:34:53 -0800

    Synch changes for account persistence

--- 6.SOAP/sample-project/account/account-ejb/src/main/java/edu/depaul/cdm/se/account/persistence/Account.java
+++ 6.SOAP/sample-project/account/account-ejb/src/main/java/edu/depaul/cdm/se/account/persistence/Account.java
@@ -1,18 +1,22 @@
 package edu.depaul.cdm.se.account.persistence;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "ACCOUNTS")
 public class Account implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    @Column(name = "nm")
     private String name;
 
     public String getName() {
