7055cb9a94222baf0975967402de9b4b09bb2f04
shinabro <kyu_23@yahoo.com>
2013-12-28 05:38:45 -0800

    Removed duplicate directory

--- L1-javapersistence/src/main/resources/META-INF/create.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE BOOK (ID BIGINT NOT NULL, title varchar(255), isbn VARCHAR(20), price float, PRIMARY KEY (ID))
-
--- L1-javapersistence/src/main/resources/META-INF/load.sql
+++
@@ -1,3 +0,0 @@
-insert into BOOK(id, title, price) values (1, 'To Kill a MockingBird', 9.99)
-insert into BOOK(id, title, price) values (2, 'James Bond', 19.99)
-insert into BOOK(id, title, price) values (3, 'Intro to JEE7', 49.99)
