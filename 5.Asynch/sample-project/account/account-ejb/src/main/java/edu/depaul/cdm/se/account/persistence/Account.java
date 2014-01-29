f40accba2eab4a71184bd44da9f9d19ece92a0b0
Ken Yu <kyu_23@yahoo.com>
2014-01-29 03:27:58 -0600

    Updated for sequence starting to account for initial data

--- 5.Asynch/sample-project/account/account-ejb/src/main/java/edu/depaul/cdm/se/account/persistence/Account.java
+++ 5.Asynch/sample-project/account/account-ejb/src/main/java/edu/depaul/cdm/se/account/persistence/Account.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -13,7 +14,8 @@ import javax.persistence.Table;
 public class Account implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator="Seq")
+    @SequenceGenerator(name="Seq",sequenceName="INV_SEQ", allocationSize=5, initialValue = 10)
     private Long id;
     
     @Column(name = "nm")
