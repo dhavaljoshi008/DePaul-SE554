1c2d5dc478801f2a184ec019887f245244bcd573
Ken Yu <kyu_23@yahoo.com>
2014-03-09 18:10:58 -0500

    Updated to include client

--- 7.REST/sample-project/account/account-web/src/main/java/edu/depaul/cdm/se/account/service/AccountREST.java
+++ 7.REST/sample-project/account/account-web/src/main/java/edu/depaul/cdm/se/account/service/AccountREST.java
@@ -1,20 +1,32 @@
 package edu.depaul.cdm.se.account.service;
 
 import edu.depaul.cdm.se.account.persistence.Account;
-import edu.depaul.cdm.se.account.service.AccountServiceRemote;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("account")
 public class AccountREST  {
 
     @EJB
-    private AccountServiceRemote accountRemote;    
+    private AccountServiceRemote accountRemote;
+    
+    @POST
+    @Path("/add")
+    public Response openAccount(@FormParam("name") String name, 
+            @FormParam("init_bal") float initialBalance) throws NegativeBalanceException {
+        long accntNbr =  accountRemote.openAccount(name, initialBalance);
+        
+        return Response.status(Response.Status.OK)
+                .entity("account_num: " + accntNbr).build();
+    }
     
     @GET
     @Produces({"application/xml", "application/json"})
